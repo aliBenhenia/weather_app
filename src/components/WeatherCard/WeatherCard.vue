596e9ec8c5f4002e8ace983ea98d89d850505b66
aliban <aliban@gmail.com>
2024-11-11 17:22:15 +0100

    feat(ui): add Figma-inspired weather card design

--- src/components/WeatherCard/WeatherCard.vue
+++ src/components/WeatherCard/WeatherCard.vue
@@ -1,20 +1,85 @@
 <template>
-    <div class="weather-card">
-        start layout
+  <div class="weather-card">
+    <div class="header">
+      Casablanca
+      <span class="icon-container"><img class="settings-icon" :src="settingsIcon" alt="Settings"></span>
     </div>
+    <div class="date-time">
+      Friday, November 8, 2024<br>9:09 PM
+    </div>
+    <div class="temperature">
+  <div class="temp-left">
+    <img src="https://img.icons8.com/ios/50/000000/partly-cloudy-day.png" alt="Weather Icon">
+    <div class="temp-degree">19°</div>
+  </div>
+  <div class="temp-info">
+    <div class="weather-description">Partly Cloudy</div>
+    <div class="feels-like">
+      <span>
+        Feels like: 21°
+      </span>
+    </div>
+  </div>
+</div>
+
+    <div class="conditions">
+      <div>Humidity<br><strong>40%</strong></div>
+      <div>Wind<br><strong>1 km/h</strong></div>
+      <div>Precipitation<br><strong>15%</strong></div>
+      <div>AQI<br><strong>141</strong></div>
+    </div>
+    <div class="aqi">
+      AQI
+      <div class="aqi-bar" style="width: 50%;"></div>
+      300
+    </div>
+    <div class="forecast-tabs">
+      <div class="tab" :class="{ active: activeTab === 'hourly' }" @click="toggleTab('hourly')">Hourly Forecast</div>
+      <div class="tab" :class="{ active: activeTab === 'daily' }" @click="toggleTab('daily')">7-Day Forecast</div>
+    </div>
+    <div class="hourly-forecast" v-if="activeTab === 'hourly'">
+      <div class="hour" v-for="(hour, index) in hourlyForecast" :key="index">
+        <div>{{ hour.time }}</div>
+        <img :src="hour.icon" alt="Weather Icon">
+        <div>{{ hour.temp }}°</div>
+      </div>
+    </div>
+    <div class="hourly-forecast" v-if="activeTab === 'daily'">
+      <div class="hour" v-for="(hour, index) in hourlyForecast" :key="index">
+        <div>{{ hour.time - 1 }}</div>
+        <img :src="hour.icon" alt="Weather Icon">
+        <div>{{ hour.temp }}°</div>
+      </div>
+    </div>
+    
+  </div>
 </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        
-      };
+
+<script>
+import settingsIcon from '@/assets/settings-4-fill.svg';
+export default {
+  data() {
+    return {
+      activeTab: 'hourly',
+      hourlyForecast: [
+        { time: '9:00', icon: 'https://img.icons8.com/ios/24/000000/partly-cloudy-day.png', temp: 19 },
+        { time: '10:00', icon: 'https://img.icons8.com/ios/24/000000/sun.png', temp: 21 },
+        { time: '11:00', icon: 'https://img.icons8.com/ios/24/000000/partly-cloudy-day.png', temp: 22 },
+        { time: '12:00', icon: 'https://img.icons8.com/ios/24/000000/rain.png', temp: 22 },
+        { time: '13:00', icon: 'https://img.icons8.com/ios/24/000000/partly-cloudy-day.png', temp: 22 },
+        { time: '14:00', icon: 'https://img.icons8.com/ios/24/000000/sun.png', temp: 23 },
+        { time: '15:00', icon: 'https://img.icons8.com/ios/24/000000/sun.png', temp: 23 },
+        { time: '16:00', icon: 'https://img.icons8.com/ios/24/000000/partly-cloudy-day.png', temp: 21 },
+      ],
+      settingsIcon,
+    };
+  },
+  methods: {
+    toggleTab(tab) {
+      this.activeTab = tab;
     },
-    methods: {
-      
-    },
-  };
-  </script>
-  
+  },
+};
+</script>
+
 <style  src="./WeatherCard.css"></style>
