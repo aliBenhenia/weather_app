083b17965a4ff570dc4a461af4b1d27e5fcc2a50
ali <alibenhenia1@gmail.com>
2024-11-11 11:53:22 +0100

    feat: set up project structure and initial Vue components

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+      <WeatherCard />
+    </div>
+  </template>
+  
+  <script>
+  import WeatherCard from './components/WeatherCard/WeatherCard.vue';
+  
+  export default {
+    components: {
+      WeatherCard
+    }
+  };
+  </script>
+  
+  <style>
+  </style>
---
+++ src/components/WeatherCard/WeatherCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="weather-card">
+        start layout
+    </div>
+</template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        
+      };
+    },
+    methods: {
+      
+    },
+  };
+  </script>
+  
+<style  src="./WeatherCard.css"></style>
